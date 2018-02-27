88688737479f8c9bf33e552f161af3bea2472823
august.conwell <august.conwell@gmail.com>
2018-02-27 11:19:35 -0500

    comply with linting rules

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,14 @@
 import { configure } from '@storybook/vue';
 
-import Vue from 'vue';
-import Vuex from 'vuex'; // Vue plugins
-
+import Vue from 'vue'
+import Vuex from 'vuex' // Vue plugins
+import contenteditable from 'vue-contenteditable-directive'
 
 // Install Vue plugins.
 Vue.use(Vuex);
+Vue.use(contenteditable)
 
-function loadStories() {
+function loadStories () {
   // You can require as many stories as you need.
   require('../src/stories/components/ChatContainer');
   require('stories/components/chat-presentation/message-input/SendButton')
@@ -15,4 +16,4 @@ function loadStories() {
   require('stories/components/chat-presentation/message-input/MessageInput')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
