7bd24e9136da1022e3694fbda69344d2e752ed38
August Conwell <august.conwell@gmail.com>
2018-03-01 23:14:19 -0500

    One more message

--- src/stories/components/chat-presentation/chat-display/ChatDisplay/index.js
+++ src/stories/components/chat-presentation/chat-display/ChatDisplay/index.js
@@ -4,12 +4,12 @@ import AppStyle from 'AppStyle.vue'
 import ChatDisplay from 'components/chat-presentation/chat-display/ChatDisplay.vue'
 
 storiesOf('ChatDisplay', module)
-  .add('Received', () => ({
+  .add('Received and sent messages', () => ({
     components: { ChatDisplay, AppStyle },
     template: `
         <app-style>
-            <ChatDisplay :messages="messages" :received="true"/>
-        </app-style>        
+          <ChatDisplay :messages="messages" :received="true"/>
+        </app-style>  
     `,
     data () {
       return {
@@ -17,6 +17,10 @@ storiesOf('ChatDisplay', module)
           {
             message: 'testing 123',
             received: true
+          },
+          {
+            message: 'hey',
+            received: false
           },
           {
             message: 'hey',
