073d2dcca90ef32995ee5489c1a4998b0ab74d56
August Conwell <august.conwell@gmail.com>
2018-03-08 00:21:56 -0500

    basic login page

---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit="handleSubmit">
+    <label for="email">Email</label>
+    <input type="text" id="email" v-model="email">
+    <label for="password">Password</label>
+    <input type="password" id="password" v-model="password">
+    <input type="submit" value="Login"/>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault()
+      axios.post('https://localhost:3000/auth/login', { email: this.email, password: this.password })
+        .then(({response}) => console.log(response))
+        .catch(({response}) => console.log(response))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
